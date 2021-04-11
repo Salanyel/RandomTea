2e03bf488a1828d8ba658436bf40a05a5bdbf876
Maxime de VECCHI <maxime.devecchi@gmail.com>
2021-04-10 21:24:33 -0400

    Task/main page (#1)
    * Add the main panels
    
    * Create Buttons Bar
    
    * Add the ButtonBarButtons logic
    
    * Create tea data

---
+++ RandomTea/Assets/Scripts/UI/ButtonBarButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ButtonBarButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_panelToOpen = null;
+
+    [SerializeField]
+    private GameObject[] m_panelsToClose = null;
+
+    public void OnClick()
+    {
+        foreach(var panelToClose in m_panelsToClose)
+        {
+            panelToClose.SetActive(false);
+        }
+
+        m_panelToOpen.SetActive(true);
+    }
+}
