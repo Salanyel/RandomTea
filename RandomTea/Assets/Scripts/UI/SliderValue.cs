d23fdcbdc4e37c798c2acb14c4fdc49899b24ec0
Lea <leabouvier@free.fr>
2021-04-26 19:44:06 -0400

    I can print a TeaData in the console :)

---
+++ RandomTea/Assets/Scripts/UI/SliderValue.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+    public enum SliderType
+    {
+        Temperature = 0,
+        Caffeine
+    }
+
+    public Slider m_slider;
+    public Text m_text;
+    public SliderType m_sliderType;
+
+    private void Start()
+    {
+        SetTextValue();
+    }
+
+    public void OnValueChange()
+    {
+        SetTextValue();
+    }
+
+    private void SetTextValue()
+    {
+        switch (m_sliderType)
+        {
+            case SliderType.Temperature:
+                if (m_slider.value == 16)
+                {
+                    m_text.text = "15+";
+                }
+                else
+                {
+                    m_text.text = m_slider.value.ToString();
+                }
+                break;
+
+            case SliderType.Caffeine:
+                string caffeineValue = string.Empty;
+                int currentValue = (int)m_slider.value;
+                                
+                switch (currentValue)
+                {
+                    case 0:
+                        caffeineValue = "No caffeine";
+                        break;
+
+                    case 1:
+                        caffeineValue = "Low";
+                        break;
+
+                    case 2:
+                        caffeineValue = "Medium";
+                        break;
+
+                    case 3:
+                    default:
+                        caffeineValue = "High";
+                        break;
+                }
+                m_text.text = caffeineValue;
+                break;
+        }
+    }
+}
