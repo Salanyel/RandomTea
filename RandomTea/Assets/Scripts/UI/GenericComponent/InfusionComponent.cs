9d9ee79f9d2eacdceb3b31f362476ac7a25195ca
Xyunn <maxime.devecchi@gmail.com>
2021-05-28 21:10:21 -0400

    Adding the tea list, the pool system, the alarm buton and the button page

---
+++ RandomTea/Assets/Scripts/UI/GenericComponent/InfusionComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfusionComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_temperature = null;
+
+    [SerializeField]
+    private Text m_temperatureBase = null;
+
+    [SerializeField]
+    private Text m_infusionTime = null;
+
+    public void SetInfusion(TeaData teaData)
+    {
+        SetTemperature(teaData.m_temperature, teaData.m_temperatureBase);
+        SetInfusionTime(teaData.m_minInfusionTime, teaData.m_maxInfusionTime);
+    }
+
+    private void SetTemperature(int temperature, TemperatureBase temperatureBase)
+    {
+        string temperatureBaseString = string.Empty;
+
+        m_temperature.text = $"{temperature}";
+
+        switch (temperatureBase)
+        {
+            case TemperatureBase.Celsius:
+                temperatureBaseString = "c";
+                break;
+
+            default:
+                temperatureBaseString = "F";
+                break;
+        }
+
+        m_temperatureBase.text = temperatureBaseString;
+    }
+
+    private void SetInfusionTime(int min, int max)
+    {
+        m_infusionTime.text = $"{min} - {max} min";
+    }
+}
